8c8c9fd4f5021e0d533153e064af8e24433f8e67
RodrigoMenegat <rodrigoschuinski@gmail.com>
2026-09-17 20:46:41 -0300

    updates files with new data

--- geo/code/run.py
+++ geo/code/run.py
@@ -5,11 +5,21 @@ import d_parse_answers
 import e_make_data_json
 
 def main():
-	#a_download_csv.main()
+	a_download_csv.main()
+	print("a succesful")
+	
 	b_geomatch.main()
+	print("b succesful")
+
 	c_namefix_geojson.main()
+	print("c succesful")
+
 	d_parse_answers.main()
+	print("d succesful")
+
 	e_make_data_json.main()
+	print("e succesful")
+
 
 if __name__ == "__main__":
 	main()
