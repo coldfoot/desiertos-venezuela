ce760722f4d12273ac7146f6598c4aa9ae9aac9b
tiagombp <tiagombp@gmail.com>
2026-09-26 03:34:29 -0300

    implements hover on large units

--- mapa/script.js
+++ mapa/script.js
@@ -29,9 +29,34 @@ fetch("../geo/output/data.json").then(response => response.json()).then(data => 
 
 function init(data) {
   console.log(data);
+
+  const colors_codes = {
+    Bosque: "#657034",
+    Desierto: "#b26d36",
+    Semidesierto: "#d4ba99",
+    Semibosque: "#e0d579"
+};
+
+  const colors = {};
+  data.small_units.forEach(small_unit => {
+
+    colors[small_unit.BASIC_INFO.LEVEL_2_CODE] = colors_codes[small_unit.BASIC_INFO.CLASSIFICATION];
+
+  })
+
+  color_map = Object.entries(colors).flat();
+
+  console.log(colors);
+
+  init_map();
+
 }
 
-map.on('load', () => {
+function init_map() {
+
+  console.log(color_map);
+
+  map.on('load', () => {
 
 
   map.fitBounds([-73.3911486, 0.6493155, -56.4818190, 15.7029483] , {
@@ -61,37 +86,148 @@ map.on('load', () => {
     }
   });
 
-  map.addSource('venezuela', {
+  map.addSource('large-units', {
       'type': 'vector',
-      'url': 'mapbox://tiagombp.rzef2q0scjuf',
+      'url': 'mapbox://tiagombp.l4n4rlnd4xsh',
       'promoteId' : 'code'
   });
 
   /* camadas das small units */
-
+/*
   map.addLayer({
     'id': 'small_units_fill',
     'type': 'fill',
     'source': 'venezuela',
     'source-layer': "628134755d56856f760c",
     'paint': {
-        'fill-color' : 'hotpink',
+        'fill-color' :
+          [
+              'match',
+              ['to-string', ['get', 'code']],
+              ...color_map,
+              'transparent'
+          ],
         'fill-outline-color' : 'black'
     }
   });
+*/
 
   /* camadas das large units */
+
+  let provinciaHoveredId = null;
+
+  map.addLayer({
+    'id': 'large_units_hover',
+    'type': 'fill',
+    'source': 'large-units',
+    'source-layer': "e6ce5cc1dd5b09147e8a",
+    'paint': {
+        'fill-color' : 'white',
+        'fill-opacity': [
+          'case',
+          [
+              'boolean', 
+              ['feature-state', 'hover'], 
+              false
+          ],
+          .5,
+          0
+        ]
+      }
+  });
 
   map.addLayer({
       'id': 'large_units_border',
       'type': 'line',
-      'source': 'venezuela',
+      'source': 'large-units',
       'source-layer': "e6ce5cc1dd5b09147e8a",
       'paint': {
           'line-color' : 'black',
-          'line-width' : 2
-      }
+          'line-width' : 3
+      },
+      'filter': ['==', 'provincia', '']
   });
 
+  map.addLayer({
+      'id': 'large_units_border_hover',
+      'type': 'line',
+      'source': 'large-units',
+      'source-layer': "e6ce5cc1dd5b09147e8a",
+      'paint': {
+          'line-color' : 'black',
+          'line-opacity': [
+            'case',
+            [
+                'boolean', 
+                ['feature-state', 'hover'], 
+                false
+            ],
+            1,
+            0
+          ]
+        }
+  });
+
+  /* handlers */
+
+  function mouse_enter_handler_large(e) {
+    
+    map.getCanvas().style.cursor = 'pointer';
+
+    const place_id = e.features[0].properties.code;
+
+    if (provinciaHoveredId) {
+        map.setFeatureState(
+            { 
+              source: 'large-units',
+              sourceLayer: "e6ce5cc1dd5b09147e8a",
+              id: provinciaHoveredId
+            },
+
+            { hover : false }
+        )
+
+    }
+
+    provinciaHoveredId = place_id;
+
+    map.setFeatureState(
+        { 
+          source: 'large-units',
+          sourceLayer: "e6ce5cc1dd5b09147e8a",
+          id: provinciaHoveredId
+        },
+
+        { hover : true }
+    )
+
+  }
+
+  function mouse_leave_handler_large() {
+
+    if (provinciaHoveredId) {
+        
+      map.setFeatureState(
+
+        { 
+          source: 'large-units',
+          sourceLayer: "e6ce5cc1dd5b09147e8a",
+          id: provinciaHoveredId
+        },
+
+          { hover: false }
+      );
+    }
+
+      provinciaHoveredId = null;
+
+    }
+
+  /* inicia handlers */
+  map.on('mousemove', 'large_units_hover', mouse_enter_handler_large);
+  map.on('mouseleave', 'large_units_hover', mouse_leave_handler_large);
 
 });
+
+}
+
